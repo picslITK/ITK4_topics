3c5598e40d0ce16558199d4d7bd026f60b57b17a
Michael Stauffer <mstauff@verizon.net>
2011-07-25 10:45:44 -0400

    BUG: Restore changes for CompositTransform IO.
    The changes from gerrit patch for CompositeTransform IO were somehow
    removed in the last commit. Restored.
    
    Change-Id: Ia8fc3c01e7236edbbc6053fd93517205f0dec91f

--- Modules/Nonunit/Review/include/itkCompositeTransformReader.hxx
+++ Modules/Nonunit/Review/include/itkCompositeTransformReader.hxx
@@ -33,6 +33,9 @@ CompositeTransformReader<TScalar, NDimensions>
 {
   m_CompositeTransform = NULL;
   TransformFactory<CompositeTransformType>::RegisterTransform();
+
+  /* temporary. to be removed when final CompositeTransform IO is developed. */
+  m_ReadingCompositeTransform = true;
 }
 
 //---------------------------------------------------------
