f0f3a3a1c3a0a6935c98a6ec678a349179075bf4
Jeffrey Duda <jeff.duda@gmail.com>
2011-08-03 16:47:10 -0400

    ENH: Works with VectorImage
    Change-Id: Ic5421242365a669114faf44fc423b15825636745

--- Modules/Core/ImageFunction/include/itkVectorLinearInterpolateImageFunction.h
+++ Modules/Core/ImageFunction/include/itkVectorLinearInterpolateImageFunction.h
@@ -37,7 +37,7 @@ namespace itk
  *
  * \ingroup ImageFunctions ImageInterpolators
  *
- * \ingroup ITKImageFunction
+ * \ingroup ITK-ImageFunction
  */
 template< class TInputImage, class TCoordRep = double >
 class ITK_EXPORT VectorLinearInterpolateImageFunction:
@@ -64,8 +64,8 @@ public:
   typedef typename Superclass::RealType       RealType;
 
   /** Grab the vector dimension from the superclass. */
-  itkStaticConstMacro(Dimension, unsigned int,
-                      Superclass::Dimension);
+  //itkStaticConstMacro(Dimension, unsigned int,
+  //                    Superclass::Dimension);
 
   /** Dimension underlying input image. */
   itkStaticConstMacro(ImageDimension, unsigned int, Superclass::ImageDimension);
