518250dae8349d9f3971f8de9acf66a4ebced02a
b avants <stnava@gmail.com>
2011-08-19 18:48:03 -0400

    BUG: test is 'passing' artificially ... but at least the JH is being computed

--- Modules/Registration/Refactoring/include/itkMattesMutualInformationImageToImageObjectMetric.h
+++ Modules/Registration/Refactoring/include/itkMattesMutualInformationImageToImageObjectMetric.h
@@ -41,7 +41,7 @@ namespace itk
  *      P. Thevenaz and M. Unser
  *      IEEE Transactions in Image Processing, 9(12) December 2000.
  *
- * \ingroup ITK-RegistrationRefactoring 
+ * \ingroup ITK-RegistrationRefactoring
  */
 
 template<class TFixedImage,class TMovingImage,class TVirtualImage = TFixedImage>
@@ -61,122 +61,113 @@ public:
 
   /** Run-time type information (and related methods). */
   itkTypeMacro(MattesMutualInformationImageToImageObjectMetric, ImageToImageObjectMetric);
-  
+
   /** Type used for representing parameter values  */
   typedef typename Superclass::CoordinateRepresentationType
                                                   CoordinateRepresentationType;
   /**  Type of the parameters. */
   typedef typename Superclass::ParametersType       ParametersType;
-  typedef typename Superclass::ParametersValueType  ParametersValueType; 
-  
+  typedef typename Superclass::ParametersValueType  ParametersValueType;
+
   /** Superclass typedefs */
   typedef typename Superclass::MeasureType                 MeasureType;
   typedef typename Superclass::DerivativeType              DerivativeType;
   typedef typename Superclass::VirtualPointType            VirtualPointType;
   typedef typename Superclass::FixedImagePointType         FixedImagePointType;
   typedef typename Superclass::FixedImagePixelType         FixedImagePixelType;
-  typedef typename Superclass::FixedImageGradientType   FixedImageGradientType;
+  typedef typename Superclass::FixedImageDerivativesType   FixedImageDerivativesType;
   typedef typename Superclass::MovingImagePointType        MovingImagePointType;
   typedef typename Superclass::MovingImagePixelType        MovingImagePixelType;
-  typedef typename Superclass::MovingImageGradientType  MovingImageGradientType;
-    
+  typedef typename Superclass::MovingImageDerivativesType  MovingImageDerivativesType;
   /** Value type of the PDF */
-  typedef float PDFValueType;
-  
-  /** Typedef for the joint PDF and PDF derivatives are stored as ITK Images. */
+  typedef double PDFValueType;
+  /** Typedef for the joint PDF and marginal PDF are stored as ITK Images. */
+  typedef Image<PDFValueType,1> MarginalPDFType;
+  typedef typename MarginalPDFType::IndexType MarginalPDFIndexType;
   typedef Image< PDFValueType, 2 >            JointPDFType;
-  typedef Image< PDFValueType, 3 >            JointPDFDerivativesType;
   itkGetConstReferenceMacro(JointPDF,typename JointPDFType::Pointer);
-  itkGetConstReferenceMacro(JointPDFDerivatives,typename JointPDFDerivativesType::Pointer);  
-  
+
   itkSetClampMacro( NumberOfHistogramBins, SizeValueType,
                     5, NumericTraits< SizeValueType >::max() );
   itkGetConstReferenceMacro(NumberOfHistogramBins, SizeValueType);
-  
+
   /** Initialize the metric. Make sure all essential inputs are plugged in. */
   virtual void Initialize() throw (itk::ExceptionObject);
-  
-  /** Get both the value and derivative */
-  void GetValueAndDerivative(MeasureType & value, DerivativeType & derivative); 
-  
+
+  /** Get both the value and derivative intializes the processing.
+   *  For Mattes MI, we just compute the joint histogram / pdf here.
+   *  This implementation single-threads the JH computation but it
+   *  could be multi-threaded in the future.
+   */
+  void GetValueAndDerivative(MeasureType & value, DerivativeType & derivative);
+
+  /** Called by GetValueAndDerivative */
+  void ComputeJointPDFandMarginalPDFs();
+
   /** Compute parzen window index. */
   int ComputeParzenWindowIndex(double);
-  
+
   /** Compute the PDF Derivatives. */
   void ComputePDFDerivatives(int pdfMovingIndex,
-                             const MovingImageGradientType & movingImageGradientValue,
+                             const MovingImageDerivativesType & movingImageGradientValue,
                              double & cubicBSplineDerivativeValue,
                              ThreadIdType threadID) const;
   /** Get the value */
   MeasureType GetValue();
-  
+
 protected:
 
   MattesMutualInformationImageToImageObjectMetric();
   virtual ~MattesMutualInformationImageToImageObjectMetric();
   void PrintSelf(std::ostream & os, Indent indent) const;
-  
+
   /** Initiates multi-threading to evaluate the current metric value
    * and derivatives. */
-  virtual void GetValueAndDerivativeMultiThreadedInitiate( DerivativeType &
-							   derivativeReturn ) 
-  {
-      itk::ExceptionObject err(__FILE__, __LINE__);
-      err.SetLocation(ITK_LOCATION);
-      err.SetDescription("Not implemented");
-      throw err;
-  }
-    
+//  virtual void GetValueAndDerivativeMultiThreadedInitiate( DerivativeType &
+//							   derivativeReturn ) // use superclass
+
   /* Provide the worker routine to process each point */
   bool GetValueAndDerivativeProcessPoint(
                     const VirtualPointType &           itkNotUsed(virtualPoint),
                     const FixedImagePointType &        mappedFixedPoint,
                     const FixedImagePixelType &        fixedImageValue,
-                    const FixedImageGradientType &  fixedImageGradient,
+                    const FixedImageDerivativesType &  fixedImageDerivatives,
                     const MovingImagePointType &       mappedMovingPoint,
                     const MovingImagePixelType &       movingImageValue,
-                    const MovingImageGradientType & movingImageGradient,
+                    const MovingImageDerivativesType & movingImageDerivatives,
                     MeasureType &                      metricValueResult,
                     DerivativeType &                   localDerivativeReturn,
                     ThreadIdType                       threadID);
-  
+
   /** Default post-processing after multi-threaded calculation of
    * value and derivative.*/
-  virtual void GetValueAndDerivativeMultiThreadedPostProcess( bool doAverage ); 
-  
+//  virtual void GetValueAndDerivativeMultiThreadedPostProcess( bool doAverage ); // use superclass
+
 private:
-  
+
   //purposely not implemented
   MattesMutualInformationImageToImageObjectMetric(const Self &);
   //purposely not implemented
   void operator=(const Self &);
-  
-  typedef float *MarginalPDFType;
-  
+
   /** The fixed image marginal PDF */
-  mutable MarginalPDFType m_FixedImageMarginalPDF;
+  typename MarginalPDFType::Pointer m_FixedImageMarginalPDF;
 
   /** The moving image marginal PDF. */
-  mutable MarginalPDFType m_MovingImageMarginalPDF;
-  
+  typename MarginalPDFType::Pointer m_MovingImageMarginalPDF;
+
   /** The joint PDF and PDF derivatives. */
   typename JointPDFType::Pointer            m_JointPDF;
-  typename JointPDFDerivativesType::Pointer m_JointPDFDerivatives;
-  
+
   /** Buffer sizes of the PDF and PDF derivatives */
   SizeValueType m_JointPDFBufferSize;
-  SizeValueType m_JointPDFDerivativesBufferSize;
-  
-  /** Joint PDF and PDF Derivatives types */
+
+  /** Joint PDF types */
   typedef JointPDFType::IndexType             JointPDFIndexType;
   typedef JointPDFType::PixelType             JointPDFValueType;
   typedef JointPDFType::RegionType            JointPDFRegionType;
   typedef JointPDFType::SizeType              JointPDFSizeType;
-  typedef JointPDFDerivativesType::IndexType  JointPDFDerivativesIndexType;
-  typedef JointPDFDerivativesType::PixelType  JointPDFDerivativesValueType;
-  typedef JointPDFDerivativesType::RegionType JointPDFDerivativesRegionType;
-  typedef JointPDFDerivativesType::SizeType   JointPDFDerivativesSizeType;
-  
+
   /** Variables to define the marginal and joint histograms. */
   SizeValueType m_NumberOfHistogramBins;
   double        m_MovingImageNormalizedMin;
@@ -186,23 +177,24 @@ private:
   double        m_MovingImageTrueMin;
   double        m_MovingImageTrueMax;
   double        m_FixedImageBinSize;
-  double        m_MovingImageBinSize;    
-  
+  double        m_MovingImageBinSize;
+
   /** Typedefs for BSpline kernel and derivative functions. */
   typedef BSplineKernelFunction<3>           CubicBSplineFunctionType;
   typedef BSplineDerivativeKernelFunction<3> CubicBSplineDerivativeFunctionType;
-  
+
   /** Cubic B-Spline kernels */
   typename CubicBSplineFunctionType::Pointer             m_CubicBSplineKernel;
-  typename CubicBSplineDerivativeFunctionType::Pointer   m_CubicBSplineDerivativeKernel;  
-  
-  mutable double m_JointPDFSum;
-    
-  /** For threading */
+  typename CubicBSplineDerivativeFunctionType::Pointer   m_CubicBSplineDerivativeKernel;
+
+  double m_JointPDFSum;
+
+  /** For threading
   JointPDFType::Pointer * m_ThreaderJointPDF;
   int *m_ThreaderJointPDFStartBin;
   int *m_ThreaderJointPDFEndBin;
-  mutable double *m_ThreaderJointPDFSum;  
+  double *m_ThreaderJointPDFSum;
+  */
 };
 
 } // end namespace itk
@@ -212,3 +204,39 @@ private:
 #endif
 
 #endif
+
+
+
+
+/*
+
+  //This is of one two evaluation methods that the user may call.
+  void GetValueAndDerivative( MeasureType & valueReturn,
+                              DerivativeType & derivativeReturn)
+  {
+    //1) Do any pre-processing required for your metric. To help with
+    // threading, you can use ImageToData or Array1DToData classes,
+    // or derive your own from ObjectToData.
+
+    //2) Call GetValueAndDerivativeMultiThreadedInitiate.
+    //This will iterate over virtual image region and call your
+    // GetValueAndDerivativeProcessPoint method, see definition in
+    // base.
+    this->GetValueAndDerivativeMultiThreadedInitiate( derivativeReturn );
+
+    //3) Optionally call GetValueAndDerivativeMultiThreadedPostProcess for
+    // default post-processing, which sums up results from each thread,
+    // and optionally averages them. It then assigns the results to
+    // 'value' and 'derivative', without copying in the case of 'derivative'.
+    //Do your own post-processing as needed.
+    this->GetValueAndDerivativeMultiThreadedPostProcess( true  );//doAverage
+
+    //4) Return the value result. The derivative result has already been
+    // written to derivativeReturn.
+    valueReturn = this->GetValueResult();
+
+    //That's it. Easy as 1, 2, 3 (and 4).
+  }
+
+
+*/
