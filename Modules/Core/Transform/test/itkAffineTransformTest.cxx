07fabf68fc261bdc6c277df599f4bb32765c0baa
Michael Stauffer <mstauff@verizon.net>
2011-08-22 17:54:42 -0400

    BUG: Problem with m_Parameters getting updated.

--- Modules/Core/Transform/test/itkAffineTransformTest.cxx
+++ Modules/Core/Transform/test/itkAffineTransformTest.cxx
@@ -647,9 +647,6 @@ int itkAffineTransformTest(int, char *[])
         }
       }
 
-    paff->SetIdentity();
-    paff->Print( std::cout );
-
     /* Test UpdateTransformParameters */
     Affine3DType::DerivativeType update( paff->GetNumberOfParameters() );
     Affine3DType::ParametersType updateTruth;
@@ -666,7 +663,11 @@ int itkAffineTransformTest(int, char *[])
       {
       if( updateTruth[k] != parametersRead[k] )
         {
-        std::cout << "UpdateTransformParameters 1 failed." << std::endl;
+        std::cout << "UpdateTransformParameters 1 failed:" << std::endl;
+        std::cout << "updateTruth: " << std::endl
+                  << updateTruth << std::endl
+                  << "parametersRead: " << std::endl
+                  << parametersRead << std::endl;
         return EXIT_FAILURE;
         }
       }
@@ -683,12 +684,20 @@ int itkAffineTransformTest(int, char *[])
       {
       if( updateTruth[k] != parametersRead[k] )
         {
-        std::cout << "UpdateTransformParameters 2 failed." << std::endl;
+        std::cout << "UpdateTransformParameters 2 failed:" << std::endl;
+        std::cout << "updateTruth: " << std::endl
+                  << updateTruth << std::endl
+                  << "parametersRead: " << std::endl
+                  << parametersRead << std::endl;
         return EXIT_FAILURE;
         }
       }
 
     {
+
+    paff->SetIdentity();
+    paff->Print( std::cout );
+
     // Test SetParameters and GetInverse
     typedef itk::AffineTransform<double,2> TransformType;
     TransformType::Pointer transform = TransformType::New();
